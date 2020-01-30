bf0432d4458684fef3260043122a8317f2665c06
Lev-coder <lev2017qaz@ukr.net>
2020-01-30 15:04:45 +0200

    (-_-)

--- trekking_1.py
+++ trekking_1.py
@@ -10,11 +10,7 @@ def swap(product_list,i,j):
     product_list[i] = product_list[j]
     product_list[j] = tmp
     
-def sort_product(product_calories):
-    product_list = []
-    for product_name,calories in product_calories.items():
-        product_list.append( Product(product_name,calories) )
-
+def sort_product(product_list):
     for i in range(len(product_list)):
         for j in range( i+1,len(product_list) ):
             product_a = product_list[i]
@@ -25,17 +21,16 @@ def sort_product(product_calories):
             elif product_a.calories == product_b.calories:
                 if product_a.name > product_b.name:
                     swap(product_list,i,j)
-
     return product_list
 
 wb = xlrd.open_workbook('trekking1.xlsx')
 trekking = wb.sheet_by_index(0)
 
-product_calories = {}
+product_list = []
 for row in range(1,trekking.nrows):
     product_name = trekking.row_values(row)[0]
     calories = trekking.row_values(row)[1]
-    product_calories[product_name] = calories
+    product_list.append(Product(product_name,calories))
     
-for product in sort_product(product_calories):
+for product in sort_product(product_list):
     print( product.name )
